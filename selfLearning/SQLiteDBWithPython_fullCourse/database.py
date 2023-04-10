15f890b6136fb3ea244ad8e115879e7a759b5833
Joy He <joyhe520@gmail.com>
2023-04-10 15:25:12 -0500

    update records

--- selfLearning/SQLiteDBWithPython_fullCourse/database.py
+++ selfLearning/SQLiteDBWithPython_fullCourse/database.py
@@ -32,15 +32,16 @@ many_customers = [('Wes', 'Brown', 'wes@me.com'),
 #*query the db
 # c.execute("SELECT * FROM customers")
 
-#* query the db table with a primary key
-c.execute("SELECT rowid, * FROM customers WHERE last_name LIKE 'Br%' ")
+#! query the db table with a primary key, where clause and like wildcard
+# c.execute("SELECT rowid, * FROM customers WHERE email LIKE '%codemy.com' ")
 # print(c.fetchone())              #?fetch the first item in the table
 # print(c.fetchmany(3))            #?fetch however many
 # print(c.fetchall())              #?fetch all
 
+# items = c.fetchall()
 
-#*query each element in a row
-items = c.fetchall()
+#! query each element in a row
+
 
 # print("NAME" + "\t\t  EMAIL")
 # print("-----" + "\t\t  -----")
@@ -48,12 +49,20 @@ items = c.fetchall()
 #     print(item[0] + " " + item[1] + " \t " + item[2])
 
 
+# for item in items:
+#     print(item)
+
+
+#* update records
+c.execute("""UPDATE customers SET first_name = 'John'
+          WHERE rowid = 1
+          
+          """)
+
+c.execute("SELECT rowid, * FROM customers")
+items = c.fetchall()
 for item in items:
     print(item)
-
-
-
-
 
 
 #* commit our command 
