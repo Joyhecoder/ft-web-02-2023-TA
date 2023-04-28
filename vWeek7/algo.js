680baac215d48bfa22e795371be968fa84f2d845
Joy He <joyhe520@gmail.com>
2023-04-28 13:54:47 -0500

    algo

--- vWeek7/algo.js
+++ vWeek7/algo.js
@@ -40,12 +40,39 @@ const chunk = (arr, num) => {
     // push this array to the final array
 }
 
-chunk([1, 2, 3, 4], 2)
-chunk([1, 2, 3, 4, 5], 2)
-chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)
+// chunk([1, 2, 3, 4], 2)
+// chunk([1, 2, 3, 4, 5], 2)
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)
 
 
 
 
+//Given a string, Write a function that returns the character that is most commonly used in the string.
+// yourFunctionName('abccccddde') ---> c
+// yourFunctionName('1111112345') ---->1
 
+const yourFunctionName = (str) => {
+    let obj = {}
+    let strArr = str.split("")
+    strArr.forEach(el => {
+        if(!obj[el]){
+            obj[el] = 1
+        }else{
+            obj[el] += 1
+        }
+    })
 
+    // console.log(obj)
+    let mostFrequent = 0
+    let result 
+    for(let key in obj){
+        // console.log(obj[key])
+        if(mostFrequent < obj[key]){
+            result = key
+            mostFrequent = obj[key]
+        }
+    }
+    console.log(result)
+}
+yourFunctionName('abccccddde')
+yourFunctionName('1111112345')
