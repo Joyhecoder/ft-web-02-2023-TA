aefabc9dcf42ab8637f1d6b487a5df16ec711f49
Joy He <joyhe520@gmail.com>
2023-06-06 14:46:39 -0500

    movie app

---
+++ vWeek13/2Tuesday/hw-react-batman-hooks/src/App.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react'
+import Movies from './components/Movies'
+import api from './api'
+
+
+
+// make your api call here
+const App = () => {
+  const [movieData, setMovieData] = useState([])
+
+//https://www.omdbapi.com/?i=tt3896198&apikey=530572a3
+
+  useEffect(() => {
+   const initalFetchMovieData = async () => {
+    try {
+      let response = await fetch(`http://www.omdbapi.com/?s=batman&apikey=${api}`)
+      let data = await response.json()
+      // console.log(data)
+      setMovieData(data.Search)
+      console.log(movieData)
+    } catch (error) {
+      console.log(error)
+    }
+    
+   }
+   initalFetchMovieData()
+   console.log(movieData)
+  }, [])
+  
+  return (
+    <>
+      <Movies allMovies = {movieData} />
+    </>
+  )
+}
+
+export default App
